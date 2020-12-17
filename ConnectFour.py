8673bfc30e9156dda694c42071329adecd296dd7
Lucas Abalos <lucas2001abalos@gmail.com>
2020-12-17 11:31:28 -0300

    Interfaz comenzada, dibujo tablero

--- ConnectFour.py
+++ ConnectFour.py
@@ -1,7 +1,16 @@
 import numpy
+import pygame
+import sys
 
 ROW_COUNT = 6
 COLUMN_COUNT = 7
+
+SQUARE_SIZE = 100  # PIXELS
+
+RADIUS = int(SQUARE_SIZE/2 - 5)
+
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
 
 
 def creates_board():
@@ -89,37 +98,62 @@ def winning_move(board, piece):
     return False
 
 
-def main():
+def draw_board(board, screen_):
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT):
+            pygame.draw.rect(screen_, BLUE, (c * SQUARE_SIZE, r * SQUARE_SIZE + SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+            pygame.draw.circle(screen_, BLACK, (int(c * SQUARE_SIZE + SQUARE_SIZE/2), int(r * SQUARE_SIZE + SQUARE_SIZE + SQUARE_SIZE/2)), RADIUS)
 
+
+def main():
     board_1 = creates_board()  # INITIALIZE BOARD, CREATES ONE
     print_board(board_1)
     turn = 1
 
+    pygame.init()
+
+    width = COLUMN_COUNT * SQUARE_SIZE
+    height = (ROW_COUNT + 1) * SQUARE_SIZE
+
+    size = (width, height)
+
+    screen = pygame.display.set_mode(size)
+
+    draw_board(board_1, screen)
+
+    pygame.display.update()
+
     while True:
 
-        col = 10  # THIS IS A PREDEFINE TO AVOID ERRORS
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-        if turn == 1:  # ASK FOR PLAYER 1 INPUT
-            while col >= 7 or col < 0:
-                col = int(input("PLayer 1 make your selection (0,6):"))
-        if turn == 2:  # ASK FOR PLAYER 2 INPUT
-            while col >= 7 or col < 0:
-                col = int(input("Player 2 make your selection (0,6):"))
+            if event.type == pygame.MOUSEBUTTONDOWN:
 
-        if is_valid_location(board_1, col):
-            row = get_next_open_row(board_1, col)
-            drop_piece(board_1, row, col, turn)
-
-        if winning_move(board_1, turn):
-            print('Congrats Player {}, you won!!'.format(turn))
-            quit()
-
-        turn = turn % 2  # THE FOLLOWING 2 LINES LOOPS BTW PLAY1 AN PLAY2
-        turn = turn + 1
-
-        print_board(board_1)
+                # col = 10  # THIS IS A PREDEFINE TO AVOID ERRORS
+                #
+                # if turn == 1:  # ASK FOR PLAYER 1 INPUT
+                #     while col >= 7 or col < 0:
+                #         col = int(input("PLayer 1 make your selection (0,6):"))
+                # if turn == 2:  # ASK FOR PLAYER 2 INPUT
+                #     while col >= 7 or col < 0:
+                #         col = int(input("Player 2 make your selection (0,6):"))
+                #
+                # if is_valid_location(board_1, col):
+                #     row = get_next_open_row(board_1, col)
+                #     drop_piece(board_1, row, col, turn)
+                #
+                # if winning_move(board_1, turn):
+                #     print('Congrats Player {}, you won!!'.format(turn))
+                #     quit()
+                #
+                # turn = turn % 2  # THE FOLLOWING 2 LINES LOOPS BTW PLAY1 AN PLAY2
+                # turn = turn + 1
+                #
+                # print_board(board_1)
+                pass
 
 
 if __name__ == '__main__':
-
     main()
